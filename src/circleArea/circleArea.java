b3b17e37adc057297119beaeccd9569d646deb46
mschoi <mschoi@mschoi-ubuntu>
2021-03-19 12:48:44 +0900

    원

---
+++ src/circleArea/circleArea.java
@@ -0,0 +1,18 @@
+package circleArea;
+
+public class circleArea {
+
+	public static void main(String[] args) {
+		String toolName = "JDK";
+		
+		final double PI = 3.14;
+		
+		double radius = 10.0;
+		double circelArea = radius * radius * PI;
+		
+		System.out.println("cirecle Area =" + circelArea);
+		System.out.println(toolName);
+		
+	}
+
+}
